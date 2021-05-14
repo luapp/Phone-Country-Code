5ad862ef092a9c80bb07a1ed4bb7676c9deda845
Paul Le Gall <darky.dev@outlook.com>
2021-05-15 00:24:38 +0800

    add basic template

--- Country_code.ts
+++ Country_code.ts
@@ -1,9 +1,74 @@
-function Country_code() {
-    const test = [
-        {
-            "id": 0, "": ["", ""],
-        },
-    ]
-    console.log("code")
+const Country_code = {
+    "0": {"Country": "Afghanistan", "Code": "93"},
+    "1": {"Country": "Albania", "Code": "355"},
+    "2": {"Country": "Algeria", "Code": "213"},
+    "3": {"Country": "American Samoa", "Code": "1-684"},
+    "4": {"Country": "Andorra", "Code": "376"},
+    "5": {"Country": "Angola", "Code": "244"},
+    "6": {"Country": "Anguilla", "Code": "1-264"},
+    "7": {"Country": "Antarctica", "Code": "672"},
+    "8": {"Country": "Antigua and Barbuda", "Code": "1-268"},
+    "9": {"Country": "Argentina", "Code": "54"},
+    "10": {"Country": "Armenia", "Code": "374"},
+    "11": {"Country": "Aruba", "Code": "297"},
+    "12": {"Country": "Australia", "Code": "61"},
+    "13": {"Country": "Austria", "Code": "43"},
+    "14": {"Country": "Azerbaijan", "Code": "994"},
+    "15": {"Country": "Bahamas", "Code": "1-242"},
+    "16": {"Country": "Bahrain", "Code": "973"},
+    "17": {"Country": "Bangladesh", "Code": "880"},
+    "18": {"Country": "US", "Code": "01"},
+    "19": {"Country": "US", "Code": "01"},
+    "20": {"Country": "US", "Code": "01"},
+    "21": {"Country": "US", "Code": "01"},
+    "22": {"Country": "US", "Code": "01"},
+    "23": {"Country": "US", "Code": "01"},
+    "24": {"Country": "US", "Code": "01"},
+    "25": {"Country": "US", "Code": "01"},
+    "26": {"Country": "US", "Code": "01"},
+    "27": {"Country": "US", "Code": "01"},
+    "28": {"Country": "US", "Code": "01"},
+    "29": {"Country": "US", "Code": "01"},
+    "30": {"Country": "US", "Code": "01"},
+    "31": {"Country": "US", "Code": "01"},
+    "32": {"Country": "US", "Code": "01"},
+    "33": {"Country": "US", "Code": "01"},
+    "34": {"Country": "US", "Code": "01"},
+    "35": {"Country": "US", "Code": "01"},
+    "36": {"Country": "US", "Code": "01"},
+    "37": {"Country": "US", "Code": "01"},
+    "38": {"Country": "US", "Code": "01"},
+    "39": {"Country": "US", "Code": "01"},
+    "40": {"Country": "US", "Code": "01"},
+    "41": {"Country": "US", "Code": "01"},
+    "42": {"Country": "US", "Code": "01"},
+    "43": {"Country": "US", "Code": "01"},
+    "44": {"Country": "US", "Code": "01"},
+    "45": {"Country": "US", "Code": "01"},
+    "46": {"Country": "US", "Code": "01"},
+    "47": {"Country": "US", "Code": "01"},
+    "48": {"Country": "US", "Code": "01"},
+    "49": {"Country": "US", "Code": "01"},
+    "50": {"Country": "US", "Code": "01"},
+    "51": {"Country": "US", "Code": "01"},
+    "52": {"Country": "US", "Code": "01"},
+    "53": {"Country": "US", "Code": "01"},
+    "54": {"Country": "US", "Code": "01"},
+    "55": {"Country": "US", "Code": "01"},
+    "56": {"Country": "US", "Code": "01"},
+    "57": {"Country": "US", "Code": "01"},
+    "58": {"Country": "US", "Code": "01"},
+    "59": {"Country": "US", "Code": "01"},
+    "60": {"Country": "US", "Code": "01"},
+    "61": {"Country": "US", "Code": "01"},
+    "62": {"Country": "US", "Code": "01"},
+    "63": {"Country": "US", "Code": "01"},
+    "64": {"Country": "US", "Code": "01"},
+    "65": {"Country": "US", "Code": "01"}
 }
-Country_code()
+//console.log(Country_code[0].Code)
+console.log(Country_code)
+
+//export default Country_code
+
+//require modules
